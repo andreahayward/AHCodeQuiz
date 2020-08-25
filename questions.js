d9b0e40dbd2dfe9fa9c4cdf17fbdc52764789d12
Andrea Hayward <andrea.teofilo@yahoo.com>
2020-08-25 08:57:21 -0400

    updating html pages and adding questions.js page

---
+++ questions.js
@@ -0,0 +1,32 @@
+var questions = [
+    {
+        question: "In the Drop Ship retailer flow, what does the retailer send the supplier?",
+        answers: {
+            a: "Inventory",
+            b: "Orders",
+            c: "Shipments",
+            d: "Nothing"
+        },
+        correctAnswer: 'b'
+    },
+    {
+        question: "At a bare minimum, how often should suppliers update their inventory for retailers?",
+        answers: {
+            a: "Once a Day",
+            b: "Once a Week",
+            c: "Twice a Week",
+            d: "Once a Month"
+        },
+        correctAnswer: 'a'
+    },
+    {
+        question: "When a supplier replenishes stock to a company/business, instead of an individual customer, what is that considered?",
+        answers: {
+            a: "Nothing, there is no special term for this",
+            b: "Sending Stock",
+            c: "Business to Business (B2B)",
+            d: "Inventory Management"
+        },
+        correctAnswer: 'c'
+    },
+]
